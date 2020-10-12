08d1b9075a3a319fa9980247ef90c12f571bc76f
Dariusz Mrugala <dariuszmrugala@gmail.com>
2020-10-12 19:49:34 +0200

    dummy mock only to run dummy game

--- tictactoe.py
+++ tictactoe.py
@@ -13,44 +13,60 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    # return [[EMPTY, EMPTY, EMPTY],
+    #         [EMPTY, EMPTY, EMPTY],
+    #         [EMPTY, EMPTY, EMPTY]]
+
+    return [[X, X, EMPTY],
+            [O, O, EMPTY],
+            [EMPTY, O, X]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    # raise NotImplementedError
-    # print(board)
-    # print(any(sublist for sublist in board)) # this not work
-    any_non_empty = False
-    for sublist in board:
-        # print(sublist)
-        # print(any(sublist))
-        if any(sublist):
-            any_non_empty = True
-    
-    if not any_non_empty:
-        print('not any_non empty')
+
+    if not any(element for sublist in board for element in sublist):
         return X
 
-    return X # mock
+    count_of_X = 0
+    count_of_O = 0
+    for row in board:
+        count_of_X += row.count(X)
+        count_of_O += row.count(O)
+    if count_of_X > count_of_O:
+        return O
+    else:
+        return X
+
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    x = {(0,2), (1,2), (2,0)}
+    print(x)
+    return x
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    if board[action[0]][action[1]] != EMPTY:
+        raise NameError('HiThere')('this action is not allowed in this board')
+    
+    result = board
+    
+    result[action[0]][action[1]] = player(board)
+
+    return result
+        
+    
 
 
 def winner(board):
@@ -66,7 +82,7 @@ def terminal(board):
     Returns True if game is over, False otherwise.
     """
     # raise NotImplementedError
-    return True
+    return False
 
 
 def utility(board):
@@ -80,4 +96,5 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    return (0,2)
